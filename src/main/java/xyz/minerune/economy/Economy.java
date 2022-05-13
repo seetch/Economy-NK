2baa4ba22ae7808b58bef46e9094bbc45b1a6aba
hteppl <hteppl.dev@gmail.com>
2022-05-14 00:00:51 +0300

    Update Economy.java

--- src/main/java/xyz/minerune/economy/Economy.java
+++ src/main/java/xyz/minerune/economy/Economy.java
@@ -1,6 +1,24 @@
 package xyz.minerune.economy;
 
+import cn.nukkit.Server;
+import cn.nukkit.command.Command;
 import cn.nukkit.plugin.PluginBase;
 
+import java.util.Arrays;
+
 public class Economy extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        //todo: register provider
+        registerCommands();
+    }
+
+    private void registerCommands() {
+        Command[] commands = new Command[]{
+            //todo: add commands
+        };
+
+        Server.getInstance().getCommandMap().registerAll("economy", Arrays.asList(commands));
+    }
 }
